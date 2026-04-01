4a74e57716013efb6cd75a070660975d4d709d48
Eric Oliveira <ericoliveirajs@gmail.com>
2026-04-01 19:12:42 -0300

    fix(app): corrigir injeção de dependência isolando controller em seu módulo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,17 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LeadsController } from '../leads/leads.controller';
+import { LeadsModule } from '../leads/leads.module'; // Importando o Módulo!
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // O Render vai injetar essa URL automaticamente
-      url: process.env.DATABASE_URL, 
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      // synchronize: true cria as tabelas automaticamente (ótimo para o MVP da sua mãe)
-      synchronize: true, 
+      synchronize: true,
       ssl: true,
       extra: {
         ssl: {
@@ -20,8 +18,9 @@ import { LeadsController } from '../leads/leads.controller';
         },
       },
     }),
+    LeadsModule, // <-- Adicionando o módulo aqui
   ],
-  controllers: [AppController, LeadsController],
+  controllers: [AppController], // <-- LeadsController removido daqui!
   providers: [AppService],
 })
 export class AppModule {}
